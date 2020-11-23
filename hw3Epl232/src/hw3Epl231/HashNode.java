2c81e496c31e9bed2562d456bb8cde84df7c5b4a
sohanassa <snassa01@ucy.ac.cy>
2020-11-23 21:44:14 +0200

    created hashNode

---
+++ hw3Epl232/src/hw3Epl231/HashNode.java
@@ -0,0 +1,27 @@
+package hw3Epl231;
+
+public class HashNode<K extends Integer, V> {
+	private K key;
+	private V value;
+
+	public HashNode(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
--- hw3Epl232/src/hw3Epl231/HashTable.java
+++ hw3Epl232/src/hw3Epl231/HashTable.java
@@ -1,12 +1,51 @@
 package hw3Epl231;
 
-public class HashTable {
+//hashtable<int,list<list<Node>>>
+public class HashTable<K extends Integer, V> {
 	private int size;
-	private int table[];
-	
+	private HashNode<K, V> table[];
+	private int used;
+
 	public HashTable() {
-		size=5;
-		table = new int[5];
+		size = 5;
+		used = 0;
+		table = new HashNode[5];
 	}
 
+	public int hashFunction(K num) {
+		return num % size;
+	}
+
+	public void rehash(int newSize) {
+		size = newSize;
+		HashNode<K, V> temp[] = new HashNode[newSize];
+		for (int i = 0; i < used; i++) {
+			temp[i] = table[i];
+		}
+		table = temp;
+	}
+
+	public int getUsed() {
+		return used;
+	}
+
+	public int getSizeOfArray() {
+		return size;
+	}
+
+	public void add(K key, V value) {
+		used++;
+		if(used>size) {
+			System.out.println("Not enough space in Hashtable, rehashing with double the previous size!");
+			rehash(size*2);
+		}
+		int position = hashFunction(key);
+		table[position] = new HashNode(key, value);
+	}
+	
+	public V getValueWithKey(K i) {
+		if(table[hashFunction(i)]==null)
+			return null;
+		return table[hashFunction(i)].getValue();
+	}
 }
