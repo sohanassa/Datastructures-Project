13fd84313f9302916cd9093630e4521d05cf388a
sohanassa <snassa01@ucy.ac.cy>
2020-11-24 02:53:49 +0200

    graph class created

---
+++ hw3Epl232/src/hw3Epl231/Graph.java
@@ -0,0 +1,10 @@
+package hw3Epl231;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+	private HashTable<Integer, ArrayList<ArrayList<GraphNode>>> h;
+	
+	
+}
---
+++ hw3Epl232/src/hw3Epl231/GraphNode.java
@@ -0,0 +1,12 @@
+package hw3Epl231;
+
+public class GraphNode {
+	private int ID;
+	private char type;
+	
+	public GraphNode(int id, char type) {
+		this.ID=id;
+		this.type=type;
+	}
+
+}
