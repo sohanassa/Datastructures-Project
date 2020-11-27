c93e53d04ee80adc275dfa402a7e4ab31cf7e66b
sohanassa <snassa01@ucy.ac.cy>
2020-11-27 23:46:22 +0200

    created print function in graph also created some helper functions for print. also moved MyEdge into separate class file and created some functions inside of it.

--- hw3Epl232/src/hw3Epl231/Graph.java
+++ hw3Epl232/src/hw3Epl231/Graph.java
@@ -10,13 +10,14 @@ public class Graph {
 	private int V;
 	private int E;
 	private HashTable<GraphNode> h;
+	private static String startVertex = "02";
 
 	public Graph() {
 		h = new HashTable();
 		V = 0;
 		E = 0;
 	}
-	
+
 	public void add(GraphNode node, int maxDist) {
 		checkNull(node);
 		h.add(node);
@@ -45,7 +46,7 @@ public class Graph {
 		}
 		// werk here
 	}
-	
+
 	public boolean vertexExists(GraphNode node) {
 		checkNull(node);
 		LinkedList list = h.getListWithID(Integer.parseInt(node.getID()));
@@ -96,66 +97,67 @@ public class Graph {
 	}
 
 	public int getIndexOfNode(GraphNode node) {
-		int index=0;
-		
-		for(int i=0; i<h.getNodes(); i++) {
-			if(!h.getListAt(i).contains(node))
-				index+=h.getListAt(i).size();
+		int index = 0;
+
+		for (int i = 0; i < h.getNodes(); i++) {
+			if (!h.getListAt(i).contains(node))
+				index += h.getListAt(i).size();
 			else
-				return index+h.getListAt(i).indexOf(node);
+				return index + h.getListAt(i).indexOf(node);
 		}
 		return -1;
 	}
 
-	public LinkedList<Integer> visitedNeighbours(boolean[] visited, LinkedList<GraphNode> closest, GraphNode node){
+	public LinkedList<Integer> visitedNeighbours(boolean[] visited, LinkedList<GraphNode> closest, GraphNode node) {
 		LinkedList<Integer> visitedNeighbours = new LinkedList<Integer>();
-		
-		for(int i=0; i<closest.size(); i++) {
-			int index=getIndexOfNode(closest.get(i));
-			if(visited[index])
+
+		for (int i = 0; i < closest.size(); i++) {
+			int index = getIndexOfNode(closest.get(i));
+			if (visited[index])
 				visitedNeighbours.add(new Integer(1));
 			else
 				visitedNeighbours.add(new Integer(0));
 		}
-		
+
 		return visitedNeighbours;
 	}
 
-	public int findNextNode(LinkedList<Integer> visitedNeighbours, LinkedList<GraphNode> closest, LinkedList<Integer> distance) {
-		
-		if(!visitedNeighbours.contains(new Integer(0)))
+	public int findNextNode(LinkedList<Integer> visitedNeighbours, LinkedList<GraphNode> closest,
+			LinkedList<Integer> distance) {
+
+		if (!visitedNeighbours.contains(new Integer(0)))
 			return -1;
-		
+
 		Integer min = new Integer(-1);
 		int index = -1;
-		for(int i=0; i<visitedNeighbours.size();i++)
-			if(visitedNeighbours.get(i).equals(new Integer(0)))
-				if(distance.get(i).compareTo(min)<0) {
+		for (int i = 0; i < visitedNeighbours.size(); i++)
+			if (visitedNeighbours.get(i).equals(new Integer(0)))
+				if (distance.get(i).compareTo(min) < 0) {
 					min = distance.get(i);
 					index = i;
 				}
 		return index;
-		
+
 	}
-	
+
 	public boolean allVisited(boolean[] visited) {
-		for(int i=0; i<visited.length; i++)
-			if(!visited[i])
+		for (int i = 0; i < visited.length; i++)
+			if (!visited[i])
 				return false;
 		return true;
 	}
-	
+
 	public int findNonVisited(boolean[] visited) {
-		for(int i=0; i<visited.length; i++)
-			if(!visited[i])
+		for (int i = 0; i < visited.length; i++)
+			if (!visited[i])
 				return i;
 		return -1;
 	}
-	
+
 	public ArrayList<MyEdge<GraphNode>> prim() {
 
-		double weight = 0;	//i didnt use this wtf im supposed to use it
-		
+		double weight = 0; // i didnt use this wtf im supposed to use it
+
 		boolean visited[] = new boolean[V];
 		for (int i = 0; i < V; i++)
 			visited[i] = false;
@@ -164,62 +166,101 @@ public class Graph {
 
 		LinkedList<GraphNode> nodes = createArrayOfNodes();
 
-		boolean allVisited=false;
-		int i=0;
-		while(!allVisited) {
-			visited[i]=true;
+		boolean allVisited = false;
+		int i = 0;
+		while (!allVisited) {
+			visited[i] = true;
 			LinkedList<GraphNode> closest = getClosestNeighbours(nodes.get(i));
 			LinkedList<Integer> distance = getDistance(nodes.get(i), closest);
 			LinkedList<Integer> visitedNeighbours = visitedNeighbours(visited, closest, nodes.get(i));
 			int index = findNextNode(visitedNeighbours, closest, distance);
-			if(index!=-1) {
+			if (index != -1) {
 				tree.add(new MyEdge(nodes.get(i), nodes.get(index), getWeight(nodes.get(i), nodes.get(index))));
-				i=index;}
+				i = index;
+			} else if (allVisited(visited))
+				allVisited = true;
 			else
-				if(allVisited(visited))
-					allVisited=true;
-				else
-					i=findNonVisited(visited);
+				i = findNonVisited(visited);
 		}
-		
+
 		return tree;
-	
+
 	}
 
-	private class MyEdge<E> {
-		E v1;
-		E v2;
-		double weight;
-
-		MyEdge(E v1, E v2, double weight) {
-			this.v1 = v1;
-			this.v2 = v2;
-			this.weight = weight;
-		}
-	}
-	
 	public int getV() {
 		return V;
 	}
-	
+
 	public int getE() {
 		return E;
 	}
-	
 
 	private void printMinimumSpanningTree(ArrayList<MyEdge<GraphNode>> tree) {
-		Queue q = new LinkedList();
-		// S O  I A M  C O N F U S S I O N 
-		
+		// queue to use for BFS
+		Queue<GraphNode> q = new LinkedList();
+
+		// check if "02" vertex exists in tree
+		if (nodeWithIdExistsInList(h.getListWithID(h.getIntValue(startVertex)), startVertex)) {
+			System.out.println("Vertex with ID 02 not in tree!");
+			System.exit(-1);
+		}
+
+		// printed array used to not get the same edge twice
+		boolean printed[] = new boolean[tree.size()]; // Initialised to false by java
+
+		// "02" vertex added
+		q.add(getVertexFromID(startVertex));
+
+		while (!q.isEmpty()) {
+			GraphNode current = q.poll();
+			// gets an edge which has the current vertex
+			MyEdge currentEdge = getEdgeWithVertexFromList(tree, current, printed);
+			// all the vertexes that have an edge with current are added to the queue
+			while (currentEdge != null) {
+				// edge is printed
+				System.out.println(currentEdge);
+				// new vertexes are added
+				q.add((GraphNode) currentEdge.getOtherObject(current));
+				// gets the next edge of current vertex if exists
+				currentEdge = getEdgeWithVertexFromList(tree, current, printed);
+			}
+		}
 	}
-	
+
+	private MyEdge getEdgeWithVertexFromList(ArrayList<MyEdge<GraphNode>> tree, GraphNode node, boolean[] printed) {
+		for (int i = 0; i < tree.size(); i++) {
+			if (printed[i] == false && tree.get(i).hasObject(node)) {
+				printed[i] = true;
+				return tree.get(i);
+			}
+		}
+		return null;
+	}
+
+	private GraphNode getVertexFromID(String id) {
+		for (int i = 0; i < h.getListWithID(h.getIntValue(id)).size(); i++) {
+			if (((GraphNode) h.getListWithID(h.getIntValue(id)).get(i)).getID().equals(id))
+				return ((GraphNode) h.getListWithID(h.getIntValue(id)).get(i));
+		}
+		return null;
+	}
+
+	private boolean nodeWithIdExistsInList(LinkedList<GraphNode> list, String id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getID().equals(id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public String toString() {
-		String s="";
-		for(int i=0; i<h.getSizeOfArray(); i++) {
-			if(h.getListAt(i)!=null)
-				for(int j=0; j<h.getListAt(i).size(); j++) {
-					s+=h.getListAt(i).get(j).toString();
-					s+="\n";
+		String s = "";
+		for (int i = 0; i < h.getSizeOfArray(); i++) {
+			if (h.getListAt(i) != null)
+				for (int j = 0; j < h.getListAt(i).size(); j++) {
+					s += ((GraphNode) h.getListAt(i).get(j)).getStringStatus();
+					s += "\n";
 				}
 		}
 		return s;
