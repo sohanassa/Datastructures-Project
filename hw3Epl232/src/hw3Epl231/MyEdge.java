62f6f7ccacf41357eb2b2a97e89251590cecdad2
sohanassa <snassa01@ucy.ac.cy>
2020-11-28 00:31:02 +0200

    oupsi forgot MyEdge

---
+++ hw3Epl232/src/hw3Epl231/MyEdge.java
@@ -0,0 +1,41 @@
+package hw3Epl231;
+
+public class MyEdge<E> {
+	E v1;
+	E v2;
+	double weight;
+
+	MyEdge(E v1, E v2, double weight) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.weight = weight;
+	}
+
+	public E getV1() {
+		return v1;
+	}
+
+	public E getV2() {
+		return v2;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public boolean hasObject(E obj) {
+		return v1.equals(obj) || v2.equals(obj);
+	}
+
+	public String toString() {
+		return v1 + " - " + v2;
+	}
+
+	public E getOtherObject(E obj) {
+		if (v1.equals(obj))
+			return v1;
+		if (v2.equals(obj))
+			return v2;
+		return null;
+	}
+}
